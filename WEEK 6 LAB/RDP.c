0b3c987dcf984ab6f3247c7b6affd6d414bfa744
chandu-2322 <111040406+chandu-2322@users.noreply.github.com>
2022-09-29 12:16:41 +0530

    Update RDP.c

--- WEEK 6 LAB/RDP.c
+++ WEEK 6 LAB/RDP.c
@@ -57,3 +57,73 @@ else
 printf("%s\tE'->^ \n",ip);
 return 1;
 }
+}
+int T()
+{
+printf("%s\tT->FT' \n",ip);
+if(F())
+{
+
+if(Tdash())
+{
+return 1;
+}
+else
+return 0;
+}
+else
+return 0;
+}
+int Tdash()
+{
+if(ip=='')
+{
+printf("%s\tT'->*FT' \n",ip);
+ip++;
+if(F())
+{
+if(Tdash())
+{
+return 1;
+}
+else
+return 0;
+}
+else
+return 0;
+}
+else
+{
+printf("%s\tT'->^ \n",ip);
+return 1;
+}
+}
+int F()
+{
+if(*ip=='(')
+{
+printf("%s\tF->(E) \n",ip);
+ip++;
+if(E())
+{
+if(*ip==')')
+{
+ip++;
+return 0;
+}
+else
+return 0;
+}
+else
+return 0;
+}
+
+else if(*ip=='i')
+{
+ip++;
+printf("%s\tF->id \n",ip);
+return 1;
+}
+else
+return 0;
+}
